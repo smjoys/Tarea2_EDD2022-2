6970690f7ba9318b39e0b04763112984231bceea
MatiasFlores <mafloresgu@gmail.com>
2022-11-25 19:39:54 -0300

    2

--- src/ArbolG.java
+++ src/ArbolG.java
@@ -1,43 +1,177 @@
+
 import java.util.StringTokenizer;
+import java.util.*;
 
-public class ArbolG {
-    public class NodoArbol{
-        String data;
-        NodoArbol son;
-        NodoArbol brother;
 
-        public NodoArbol(String tdata, NodoArbol tson, NodoArbol tbro) {
-            data = tdata;
-            son = tson;
-            brother = tbro;
+    class ArbolG {
+        private NodoArbol root;
+
+
+        //Contructor ArbolG
+        public ArbolG(){
+            root=null;
         }
-    }
-    private NodoArbol root;
 
-    public ArbolG() {
-        this.root = null;
+
+
+
+
+
+
+        ///////////////////////////////////////////////////////////// 1) Calcular Altura
+
+        public int CalcularAltura(){
+            if(root==null){
+                return 0;
+            }else{
+                return CalcularAltura(root);
+            }
+        }
+
+        private int CalcularAltura(NodoArbol nodo) {
+
+            int der=1,izq=1;
+            if (nodo == null) {
+                return 0;
+            }else{
+
+                if(nodo.hijo!=null){
+                    izq+=CalcularAltura(nodo.hijo);
+                }
+
+                if(nodo.hermano!=null){
+                    der+=CalcularAltura(nodo.hermano);
+                }
+
+                if(izq>der){
+                    return izq;
+                }else{
+                    return der;
+                }
+
+            }
+        }
+
+
+
+
+        ///////////////////////////////////////////////////////////////// 2) Peso
+
+        int Peso() {
+            return Peso(root);
+        }
+
+        private int Peso(NodoArbol nodo) {
+            if (nodo ==null){
+                return 0;
+            } else{
+                return 1 + (Peso(nodo.hijo) +Peso(nodo.hermano));
+            }
+        }
+
+
+
+        ///////////////////////////////////////////////////////////////// 3)String Path
+
+
+
+
+
+
+        ///////////////////////////////////////////////////////////////////4) NewNode
+        public boolean AgregarNodo(String elPath,String elDato) {
+            if (root == null) {
+                root = new NodoArbol(elDato, null, null);
+                return true;
+            } else {
+                NodoArbol tmp = buscaNodo(elPath);
+                if (tmp == null) {
+                    return false;
+                } else {
+                    return AgregaHermano(tmp, elDato);
+                }
+            }
+        }
+
+
+        private boolean AgregaHermano(NodoArbol elPadre, String elDato){
+            NodoArbol tmp=elPadre.hijo;
+            if(tmp == null){
+                elPadre.hijo=new NodoArbol(elDato,null,null);
+                return true;
+            } else {
+                elPadre.hijo = new NodoArbol(elDato,null, elPadre.hijo);
+                return true;
+            }
+        }
+
+        /////////////////////////////////////////////////////////////////5)Delete
+
+
+        public void Delete(String path){
+            Delete(buscaNodo(path));
+        }
+
+
+        private void Delete(NodoArbol aux){
+
+        }
+
+
+
+
+
+        //////////////////////////////////////////////////////////////////6) PreOrdenInterativo
+
+        public void PreOrdenIterativo(){
+
+        }
+
+
+
+
+        //////////////////////////////////////////////////////////Metodos de ayuda
+        private NodoArbol buscaNodo(String elPath)
+        {
+            NodoArbol tmp1= root;
+            NodoArbol tmp2 = tmp1;
+            StringTokenizer path = new StringTokenizer(elPath,"/");
+            String s;
+            while(path.hasMoreTokens())
+            { s = path.nextToken();
+                while(tmp1 !=null)
+                {
+                    if(s.equalsIgnoreCase(tmp1.nombre))  break;
+                    else {tmp2 = tmp1= tmp1.hermano;}
+                }
+                if(tmp1 == null) return tmp1;
+                else {tmp2 = tmp1;tmp1 = tmp1.hijo;}
+            }
+            return tmp2;
+        }
+
+
+
+
+        public void Imprimir() {
+            imprime(root, " ");
+        }
+
+        private void imprime(NodoArbol r, String tab) {
+            if (r!=null) {
+                System.out.println(tab + r.nombre);
+                imprime(r.hijo, tab +"  ");
+                imprime(r.hermano,tab);
+            }
+        }
+
+
+
+
+
+
+
+
+
     }
 
-    public int altura(){
-        return altura(root);
-    }
-    private int altura(NodoArbol n){
-        if (n == null) {
-            return 0;
-        } else return 1 + altura(n.son);
-
-    }
-
-    public void preordenIterativo(NodoArbol n){
-        System.out.println(n.data);
-        preordenIterativo(n.son);
-        preordenIterativo(n.brother);
-    }
-    public string Path(String o){
-
-    }
-    public void newNode(Path p, String n){
-
-    }
-
-}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,28 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        ArbolG arbol= new ArbolG();
+
+
+        arbol.AgregarNodo("","a");
+        arbol.AgregarNodo("a","c");
+        arbol.AgregarNodo("a","b");
+
+        arbol.AgregarNodo("a/c","k");
+        arbol.AgregarNodo("a/c","j");
+        arbol.AgregarNodo("a/b","h");
+        arbol.AgregarNodo("a/b","e");
+
+
+        arbol.AgregarNodo("a/b/h","m");
+        arbol.AgregarNodo("a/b/h","l");
+
+        arbol.Imprimir();
+        System.out.println("////////////////////////////////////////////////////");
+
+        System.out.println("La altura del arbol es: " +arbol.CalcularAltura());
+        System.out.println("El peso del arbol es: " + arbol.Peso());
+
+
     }
-    String hola;
 }
